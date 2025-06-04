bf2d137fbfe7b0bed5df6c58c4bb4b1cf51bbe4f
Manjunatha <k.manjunatha1518@gmail.com>
2025-06-04 16:46:47 +0530

    MissingNumberInAnArray

--- DSA/src/main/java/com/logic/DSA/Arrays/Brute/FindTheMissingNumberInAnArray.java
+++ DSA/src/main/java/com/logic/DSA/Arrays/Brute/FindTheMissingNumberInAnArray.java
@@ -1,4 +1,22 @@
 package com.logic.DSA.Arrays.Brute;
 
 public class FindTheMissingNumberInAnArray {
+
+    private static void missingNumber(int[] arr) {
+        int n = arr.length;
+        int x = arr[0];
+        for(Integer i : arr){
+            if(i != x){
+                System.out.println("THE MISSING NUMBER IS : "+ x);
+                return;
+            }else
+                x++;
+        }
+    }
+    public static void main(String[] args){
+        int arr[] = {98,99,101,102,103,104};
+        missingNumber(arr);
+    }
+
+
 }
