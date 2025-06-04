abc406e8df12c07c828042f62d88b386be56359e
Manjunatha <k.manjunatha1518@gmail.com>
2025-06-04 15:51:28 +0530

    Array Problems

--- DSA/src/main/java/com/logic/DSA/Arrays/Brute/LeadersInAnArray.java
+++ DSA/src/main/java/com/logic/DSA/Arrays/Brute/LeadersInAnArray.java
@@ -1,4 +1,36 @@
 package com.logic.DSA.Arrays.Brute;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeadersInAnArray {
+    private static void checkLeaders(int[] arr) {
+        int index = 0;
+        int n = arr.length;
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            Boolean isLeader = true;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] < arr[j]) {
+                    isLeader = false;
+                    break;
+                }
+            }
+            if (isLeader)
+                result.add(arr[i]);
+
+        }
+
+        System.out.println("THE LEADERS IN AN ARRAY ARE");
+        for(Integer ele : result)
+            System.out.println(ele);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 34, 67, 8, 6, 4, 9, 0, 1, 3, 6, 27, 8, 9};
+
+        checkLeaders(arr);
+    }
+
+
 }
--- DSA/src/main/java/com/logic/DSA/Arrays/Brute/LeftRotateByOnePlace.java
+++ DSA/src/main/java/com/logic/DSA/Arrays/Brute/LeftRotateByOnePlace.java
@@ -1,4 +1,24 @@
 package com.logic.DSA.Arrays.Brute;
 
 public class LeftRotateByOnePlace {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 34, 67, 8, 6, 4, 9, 0, 1, 3, 6, 27, 8, 9};
+        leftRotateByOnePlace(arr);
+    }
+
+    private static void leftRotateByOnePlace(int[] arr) {
+        int leftElement = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = leftElement;
+
+        System.out.println("ARRAY AFTER LEFT ROTATING");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+
 }
