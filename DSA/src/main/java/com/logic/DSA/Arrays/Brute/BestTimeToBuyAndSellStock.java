d0bf78796dcee54b838c00fc0c1f6fa09de18fe2
Manjunatha <k.manjunatha1518@gmail.com>
2025-06-14 15:41:24 +0530

    DSA

--- DSA/src/main/java/com/logic/DSA/Arrays/Brute/BestTimeToBuyAndSellStock.java
+++ DSA/src/main/java/com/logic/DSA/Arrays/Brute/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,31 @@
 package com.logic.DSA.Arrays.Brute;
 
 public class BestTimeToBuyAndSellStock {
+    private static void BuyAndSellStock(int[] arr) {
+        int left = 0;
+        int right = 1;
+        int high = arr.length-1;
+        int max = -1;
+        int buy =-1;
+        int sell = -1;
+        while(right<high){
+            if(arr[left]<arr[right]){
+              buy = left;
+              sell = right;
+              max = Math.max(max, arr[right] - arr[left]);
+              right++;
+            }else{
+                left = right;
+                right++;
+            }
+        }
+        System.out.println("BEST TIME TO BUY AND SELL STOCK IS : BUY :" +
+                " "+buy+" - SELL : "+sell+" AND MAXIMUM PROFIT IS : "+ max);
+    }
+    public static void main(String[] args){
+        int[] arr = {7,1,2,5,6,9,6};
+        BuyAndSellStock(arr);
+    }
+
+
 }
--- DSA/src/main/java/com/logic/DSA/Arrays/Brute/MajorityElement.java
+++ DSA/src/main/java/com/logic/DSA/Arrays/Brute/MajorityElement.java
@@ -1,4 +1,23 @@
 package com.logic.DSA.Arrays.Brute;
 
 public class MajorityElement {
+
+    public static void main(String[] args){
+        int[] arr = {1,3,4,2,2,2,1,1};
+        int mid = arr.length/2;
+
+        for(int i =0;i<arr.length;i++){
+            int count =0;
+            for(int j =0;j<arr.length;j++){
+                if(arr[j] == arr[i]){
+                    count++;
+                }
+            }
+            if(count >= mid) {
+                System.out.println("Majority element is : " + arr[i]);
+                return;
+            }
+        }
+        System.out.println("There's no majority element");
+    }
 }
