1f06753fe47dd684e83c951078fd99f8c62daeec
Manjunatha <k.manjunatha1518@gmail.com>
2025-06-14 17:58:22 +0530

    DSA BRUTE

--- DSA/src/main/java/com/logic/DSA/Arrays/Brute/MaximumSubArraySum.java
+++ DSA/src/main/java/com/logic/DSA/Arrays/Brute/MaximumSubArraySum.java
@@ -2,16 +2,23 @@ package com.logic.DSA.Arrays.Brute;
 
 public class MaximumSubArraySum {
     private static void maxSum(int[] arr) {
-
         int n = arr.length;
-        int sum = 0;
+        int maxSum =0;
         for(int i =0;i<n;i++){
-
+            for(int j = i+1;j<arr.length;j++){
+                int sum =0;
+                for(int k = i;k<j;k++){
+                    sum+=arr[k];
+                }
+                maxSum = Math.max(maxSum,sum);
+            }
         }
+        System.out.println("MAXIMUM SUB ARRAY SUM IS : "+ maxSum);
+
 
     }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 34, 67, 8, 6, 4, 9, 0, 1, 3, 6, 27, 8, 9};
+        int[] arr = {1, -2, 34, -67, 8, 6, 4, -9, 0, 1, 3, -6, -27, 8, 9};
 
         maxSum(arr);
     }
