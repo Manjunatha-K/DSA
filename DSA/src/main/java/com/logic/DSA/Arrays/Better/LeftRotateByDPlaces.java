048b930aa73d89bcf44c80235d982a74ec2aa955
Manjunatha <k.manjunatha1518@gmail.com>
2025-06-19 23:16:09 +0530

    DSA ARRAY

--- DSA/src/main/java/com/logic/DSA/Arrays/Better/LeftRotateByDPlaces.java
+++ DSA/src/main/java/com/logic/DSA/Arrays/Better/LeftRotateByDPlaces.java
@@ -1,4 +1,30 @@
 package com.logic.DSA.Arrays.Better;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class LeftRotateByDPlaces {
+    public static void main(String[] args) throws NumberFormatException{
+        int[] arr ={1,2,3,4,5,6,7,8,9,10};
+        int n = arr.length;
+        final int d = 15%n;
+        int[] arr1 = Arrays.stream(arr).limit(d).
+                map( i ->arr[d-i]).toArray();
+
+        int[] arr2 = reverseArray(arr,d);
+        int[] res = IntStream.concat(Arrays.stream(arr1),Arrays.stream(arr2)).toArray();
+        Arrays.stream(res).mapToObj(a-> new StringBuilder(a).reverse()).forEach(i -> System.out.println(i));
+    }
+
+    private static int[] reverseArray(int[] arr, int d) {
+        int n = arr.length-1;
+        while(d<n){
+            int temp = arr[d];
+            arr[d] = arr[n];
+            arr[n] = temp;
+            d++;
+            n--;
+        }
+        return arr;
+    }
 }
--- DSA/src/main/java/com/logic/DSA/Arrays/Better/ListOfFourSums.java
+++ DSA/src/main/java/com/logic/DSA/Arrays/Better/ListOfFourSums.java
@@ -1,14 +1,13 @@
 package com.logic.DSA.Arrays.Better;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
-public class FourSum {
+public class ListOfFourSums {
     public static void main(String[] args) {
         int[] arr = {1,2,3,0,5,6,7,4,5,4,4,4,5,5};
         int target = 14;
         int[] sortedArray = Arrays.stream(arr).sorted().toArray();
+        Set<List<Integer>> res = new HashSet<>();
         int n = arr.length;
         for(int i =0;i<n;i++){
             for(int j =i+1;j<n;j++){
@@ -18,7 +17,15 @@ public class FourSum {
                     if(target == sortedArray[i]+ sortedArray[j]+sortedArray[k]+sortedArray[l]){
                         System.out.println("The values that four sum equals is : "+
                                 sortedArray[i]+"-"+sortedArray[j]+"-"+sortedArray[k]+"-"+sortedArray[l]);
-                        return;
+                        List<Integer> alist = new ArrayList<>();
+
+                        alist.add(sortedArray[i]);
+                        alist.add(sortedArray[j]);
+                        alist.add(sortedArray[k]);
+                        alist.add(sortedArray[l]);
+                        res.add(alist);
+                        k++;
+
                     }else if(target > sortedArray[i]+ sortedArray[j]+sortedArray[k]+sortedArray[l])
                         k++;
                     else
@@ -26,6 +33,8 @@ public class FourSum {
                 }
             }
         }
-        System.out.println("There is no Four Sum");
+        System.out.println("The result is : ");
+        res.stream().forEach(a -> System.out.println(a));
+
     }
 }
