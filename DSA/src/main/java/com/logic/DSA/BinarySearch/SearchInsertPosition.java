c69fb3b4103d154f50c47aa0dd6a9b58e2a9bafe
Manjunatha <k.manjunatha1518@gmail.com>
2025-06-23 22:54:58 +0530

    Search Insert position

--- DSA/src/main/java/com/logic/DSA/BinarySearch/SearchInsertPosition.java
+++ DSA/src/main/java/com/logic/DSA/BinarySearch/SearchInsertPosition.java
@@ -1,4 +1,23 @@
 package com.logic.DSA.BinarySearch;
 
 public class SearchInsertPosition {
+    private static void findInsertPosition(int[] arr, int num){
+        int low =0;
+        int high = arr.length;
+        int mid = -1;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(arr[mid]<num){
+                low = mid+1;
+            }
+            else
+                high = mid-1;
+        }
+        System.out.println("INSERT POSITION IS : "+ low);
+    }
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4,5,6,7,8,9,11};
+        int num = 10;
+        findInsertPosition(arr,num);
+    }
 }
