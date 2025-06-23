832fb98a63609865a7a3f5508e38c6b220b18778
Manjunatha <k.manjunatha1518@gmail.com>
2025-06-23 22:15:50 +0530

    BINARY SEARCH

--- DSA/src/main/java/com/logic/DSA/BinarySearch/LowerBound.java
+++ DSA/src/main/java/com/logic/DSA/BinarySearch/LowerBound.java
@@ -1,4 +1,28 @@
 package com.logic.DSA.BinarySearch;
 
+
+/*
+Smallest index such that the number at that index
+is greater or equal to the given number
+ */
 public class LowerBound {
+    private static void lowerBoundimpl(int[] arr, int target){
+        int low = 0;
+        int high = arr.length;
+        int mid = -1;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(arr[mid]>=target)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        System.out.println("The smallest index is : "+mid +" and the number is : "+arr[mid]);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,6,8,11,15,17,19,22,22,22,22,28,29,40};
+        int target = 20;
+        lowerBoundimpl(arr,target);
+    }
 }
--- DSA/src/main/java/com/logic/DSA/BinarySearch/UpperBound.java
+++ DSA/src/main/java/com/logic/DSA/BinarySearch/UpperBound.java
@@ -1,4 +1,26 @@
 package com.logic.DSA.BinarySearch;
-
+/*
+Smallest index such that the number at the index must be greater than the given number
+ */
 public class UpperBound {
+    private static void upperBound(int[] arr, int target){
+        int low = 0;
+        int high = arr.length;
+        int mid = -1;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(arr[mid]<=target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        System.out.println("THE UPPER BOUND IS AT INDEX : "+mid+" and the value is : "+ arr[mid]);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {11,14,16,18,22,26,26,26,29,30,33,35,40,46};
+        int target = 26;
+
+        upperBound(arr,target);
+    }
 }
