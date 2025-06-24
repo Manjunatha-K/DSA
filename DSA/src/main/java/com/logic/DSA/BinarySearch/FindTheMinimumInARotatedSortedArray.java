969f5ef7af4af6100b38b75b4c48b56c6e847914
Manjunatha <k.manjunatha1518@gmail.com>
2025-06-24 13:33:27 +0530

    Minimum

--- DSA/src/main/java/com/logic/DSA/BinarySearch/FindTheMinimumInARotatedSortedArray.java
+++ DSA/src/main/java/com/logic/DSA/BinarySearch/FindTheMinimumInARotatedSortedArray.java
@@ -1,4 +1,30 @@
 package com.logic.DSA.BinarySearch;
 
 public class FindTheMinimumInARotatedSortedArray {
+
+    private static void findMinimum(int[] arr) {
+        int low = 0;
+        int high = arr.length-1;
+        int mid = -1;
+        int min = Integer.MAX_VALUE;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(arr[low] < arr[high]){
+                 min = arr[low];
+                 break;
+            }
+            if(arr[mid]>arr[high]){
+                low = mid+1;
+            }else{
+                high  = mid-1;
+            }
+
+        }
+        System.out.println("The MINIMUM IS : "+ min);
+    }
+    public static void main(String[] args) {
+        int[] arr = {6,7,8,9,10,11,12,13,14,4,5};
+        findMinimum(arr);
+    }
+
 }
